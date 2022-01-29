70be10774b68d051d46e8587825360185e5d419f
gcwhitfield <gwhitfieldjr@gmail.com>
2022-01-28 22:24:34 -0500

    Add goal test art, add step limit to player

--- Assets/Code/LevelController.cs
+++ Assets/Code/LevelController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 // LevelController is responsible for the overall flow of the game. It will
 // handle things like pausing, moving between scenes, ending and beginning levels, etc.
+//
+// The gameplay is divided into two phases. In the first phase, the player is trying to
+// reach the goal. In the second phase, the player is trying to return back to the start
+// location. 
 public class LevelController : Singleton<LevelController>
 {
     // Start is called before the first frame update
@@ -12,9 +16,19 @@ public class LevelController : Singleton<LevelController>
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // called then the player starts the game
+    void Phase1Begin()
     {
-        
+        // TODO: play level begin sound?
+        // TODO: begin phase 1 music
+    }
+
+    // called when the player reaches the goal and and begins the second phase,
+    // where they must return to the beginning
+    void Phase2Begin()
+    {
+        // TODO: stop playing phase 1 music, play phase 2 music
+        // TODO: play phase 2 begin sound?
+        // TODO: 
     }
 }
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public int stepLimit;
 
     // Start is called before the first frame update
     void Start()
@@ -13,27 +13,35 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    // Moves the player based on keyboard input. The player can only move if they have enough moves left
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (stepLimit > 0)
         {
-            gameObject.transform.position = TilemapController.Instance.MovePlayer(
-                Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(0, 1, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            gameObject.transform.position = TilemapController.Instance.MovePlayer(
-                Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(-1, 0, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            gameObject.transform.position = TilemapController.Instance.MovePlayer(
-                Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(0, -1, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            gameObject.transform.position = TilemapController.Instance.MovePlayer(
-                Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(1, 0, 0));
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                gameObject.transform.position = TilemapController.Instance.MovePlayer(
+                    Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(0, 1, 0));
+                stepLimit--;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                gameObject.transform.position = TilemapController.Instance.MovePlayer(
+                    Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(-1, 0, 0));
+                stepLimit--;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                gameObject.transform.position = TilemapController.Instance.MovePlayer(
+                    Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(0, -1, 0));
+                stepLimit--;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                gameObject.transform.position = TilemapController.Instance.MovePlayer(
+                    Vector3Int.FloorToInt(gameObject.transform.position), new Vector3Int(1, 0, 0));
+                stepLimit--;
+            }
         }
     }
 }
