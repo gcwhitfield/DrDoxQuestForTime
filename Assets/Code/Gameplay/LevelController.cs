61a1542e464d0930128586148ab977e661788133
0-0-256 <cheng.cameron@gmail.com>
2022-01-30 00:15:30 -0500

    music now stops when it's supposed to like a good boy

--- Assets/Code/Gameplay/LevelController.cs
+++ Assets/Code/Gameplay/LevelController.cs
@@ -58,8 +58,6 @@ public class LevelController : Singleton<LevelController>
         Debug.Log("Beginning Phase 2");
         phase = GamePhase.PHASE2;
         
-        Music.setParameterByName("hasCloneAppeared", 1);
-
         // enable the time shadow
         TimeShadow.Instance.transform.Find("Art").gameObject.SetActive(true);
 
@@ -74,6 +72,7 @@ public class LevelController : Singleton<LevelController>
     {
         if (phase == GamePhase.PHASE1)
         {
+            Music.setParameterByName("hasCloneAppeared", 1);
             TimeShadow.Instance.moves = Player.Instance.movesLog;
             Debug.Log("The player has reached the goal.");
             Phase2Begin();
@@ -84,6 +83,7 @@ public class LevelController : Singleton<LevelController>
     {
         if (phase == GamePhase.PHASE2)
         {
+            Music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
             Debug.Log("You win!");
             winScreen.SetActive(true);
         }
